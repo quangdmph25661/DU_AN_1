2d7b7fbe14fa90f94f807f232fb97d4450c40ccd
KENKI-2608\Quang <quangdmph25661@fpt.edu.vn>
2022-11-20 13:55:04 +0700

    Code_DA1

---
+++ du_an1/src/view/Application.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+/**
+ *
+ * @author Quang
+ */
+public class Application {
+
+    public static void main(String[] args) {
+
+        form_da1 fd = new form_da1();
+        fd.setLocationRelativeTo(null);
+        fd.setVisible(true);
+        fd.setVisible(false);
+        QLNVFrame qlkhf = new QLNVFrame();
+        qlkhf.setLocationRelativeTo(null);
+        qlkhf.setVisible(true);
+    }
+}
